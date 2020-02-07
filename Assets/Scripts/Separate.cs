0c1bcb0aef6e279d9e45bb1d57c6e0eabce0fc85
Dai Ibrahim <33212069+Dai-Ibrahim@users.noreply.github.com>
2020-02-07 02:14:01 -0700

    Assignment Submission

---
+++ Assets/Scripts/Separate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Separate : MonoBehaviour
+{
+     public Kinematic character;
+    public Kinematic[] targets;
+
+    private float maxAcceleration = 150f;
+    private float threshold = 55f;
+    private float decayCoefficient = 1000f;
+
+
+    public SteeringOutput getSteering()
+    {
+        SteeringOutput result = new SteeringOutput();
+
+        for (int i = 0; i < targets.Length; i++)
+        {
+			Vector3 direction = targets[i].transform.position - character.transform.position;
+            float distance = direction.magnitude;
+			Debug.Log(distance);
+
+            if (distance < threshold)
+            {
+                //Get strength of repulsion using inverse square law\
+                float strength = Mathf.Min(decayCoefficient / (distance * distance), maxAcceleration);
+
+                direction = direction.normalized;
+                result.linear += strength * direction;
+            }
+        }
+        return result;
+    }
+}
---
+++ Assets/Scripts/menuControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class menuControl : MonoBehaviour
+{
+    public void NextScene()
+    {
+
+		switch(this.gameObject.name){
+		case "Development":
+			SceneManager.LoadScene("Scene1");
+			break;
+		case "Production":
+			SceneManager.LoadScene("Scene2");
+			break;
+		}
+    }
+}
